378bb8a8c9787e2dbd1fcc99545c78555ee1e8b7
sunyuecheng <sunyuecheng@sina.cn>
2019-03-10 17:06:42 +0800

    Update pom.xml

--- install/alpsdev-init/init.sh
+++ install/alpsdev-init/init.sh
@@ -7,7 +7,29 @@
 
 source ./VERSION
 
-mvn install:install-file -Dfile=./alps-bean-${MODULE_VERSION}.jar -DgroupId=com.projn.alps -DartifactId=alps-bean -Dversion=${MODULE_VERSION} -Dpackaging=jar
-mvn install:install-file -Dfile=./alps-dao-${MODULE_VERSION}.jar -DgroupId=com.projn.alps -DartifactId=alps-dao -Dversion=${MODULE_VERSION} -Dpackaging=jar
-mvn install:install-file -Dfile=./alps-test-${MODULE_VERSION}.jar -DgroupId=com.projn.alps -DartifactId=alps-test -Dversion=${MODULE_VERSION} -Dpackaging=jar
-mvn install:install-file -Dfile=./alps-common-${MODULE_VERSION}.jar -DgroupId=com.projn.alps -DartifactId=alps-common -Dversion=${MODULE_VERSION} -Dpackaging=jar
+# nexus_repo_url like 'http://192.168.37.XXX:8082'
+nexus_repo_url=''
+
+src='<module>alpsmicroservice</module>'
+dst=''
+sed -i "s#$src#$dst#g" pom.xml
+
+src='<module>alpsgenerator</module>'
+dst=''
+sed -i "s#$src#$dst#g" pom.xml
+
+src='<module>alpsconfigserver</module>'
+dst=''
+sed -i "s#$src#$dst#g" pom.xml
+
+if [ "$nexus_repo_url" != "" ]; then
+    src='</project>'
+    dst="<distributionManagement>\n        <repository>\n            <id>nexus-releases</id>\n            <name>Nexus Release Repository</name>\n            <url>${nexus_repo_url}/repository/maven-releases/</url>\n        </repository>\n        <snapshotRepository>\n            <id>nexus-snapshots</id>\n            <name>Nexus Snapshot Repository</name>\n            <url>${nexus_repo_url}/repository/maven-snapshots/</url>\n        </snapshotRepository>\n    </distributionManagement>\n</project>"
+    sed -i "s#$src#$dst#g" pom.xml
+
+    mvn deploy
+else
+    mvn install
+    #mvn install:install-file -Dfile=./alps-bean/target/alps-bean-${ALPS_MODULE_VERSION}.jar -DgroupId=com.projn.alps -DartifactId=alps-bean -Dversion=${ALPS_MODULE_VERSION} -Dpackaging=jar
+fi
+
